9f5cdc4a3a0fa7cb563755e3d146f6184734c9a4
netanelC <netanel153@gmail.com>
2023-03-21 11:36:29 +0200

    fix: use container resolve instead of new

--- src/getProvider.ts
+++ src/getProvider.ts
@@ -1,17 +1,20 @@
 // import { FSProvider } from './common/providers/fSProvider';
+import { container } from 'tsyringe';
+import httpStatus from 'http-status-codes';
 import { S3Provider } from './common/providers/s3Provider';
 import { IConfigProvider, IProviderMap } from './common/interfaces';
 import { NFSProvider } from './common/providers/nfSProvider';
+import { AppError } from './common/appError';
 
 const providers: IProviderMap = {
-  'nfs': new NFSProvider(),
-  's3': new S3Provider()
+  'nfs': container.resolve(NFSProvider),
+  's3': container.resolve(S3Provider)
 };
 
 export const GetProvider = (provider: string): IConfigProvider => {
   try {
     return providers[provider.toLowerCase()];
   } catch (err) {
-    throw new Error(`Invalid config provider received: ${provider} - available values: "nfs" or "s3"`);
+    throw new AppError('', httpStatus.INTERNAL_SERVER_ERROR, `Invalid config provider received: ${provider} - available values: "nfs" or "s3"`, false);
   }
 };
