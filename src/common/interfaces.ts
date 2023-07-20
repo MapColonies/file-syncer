23256bf8109929d261197d397484381435248c43
Ron Nachum <ronnahom96@gmail.com>
2023-07-20 15:31:09 +0300

    fix: env

--- src/common/interfaces.ts
+++ src/common/interfaces.ts
@@ -20,6 +20,7 @@ export interface TaskParameters {
 }
 
 export interface S3Config {
+  type: "S3";
   accessKeyId: string;
   secretAccessKey: string;
   endpointUrl: string;
@@ -32,6 +33,7 @@ export interface S3Config {
 }
 
 export interface NFSConfig {
+  type: "NFS";
   pvPath: string;
 }
 
