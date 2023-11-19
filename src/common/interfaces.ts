500091b1b02eed89d01fd461b47ecf3b6303b2a9
netanelC <netanel153@gmail.com>
2023-11-19 10:57:49 +0200

    fix: make storage class optional

--- src/common/interfaces.ts
+++ src/common/interfaces.ts
@@ -23,7 +23,7 @@ export interface S3Config {
   forcePathStyle: boolean;
   maxAttempts: number;
   sigVersion: string;
-  storageClass: string;
+  storageClass?: string;
 }
 
 export interface NFSConfig {
