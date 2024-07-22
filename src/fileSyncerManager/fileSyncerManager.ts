7926ae2c67db8690af05429f2db3a4565a53614d
Asaf Masa <ASAFMAS@rafael.co.il>
2024-07-22 14:16:14 +0300

    chore: add log context

--- src/fileSyncerManager/fileSyncerManager.ts
+++ src/fileSyncerManager/fileSyncerManager.ts
@@ -7,7 +7,7 @@ import { withSpanAsyncV4 } from '@map-colonies/telemetry';
 import { Tracer, trace } from '@opentelemetry/api';
 import { INFRA_CONVENTIONS, THREE_D_CONVENTIONS } from '@map-colonies/telemetry/conventions';
 import { JOB_TYPE, SERVICES } from '../common/constants';
-import { ProviderManager, TaskParameters, TaskResult } from '../common/interfaces';
+import { LogContext, ProviderManager, TaskParameters, TaskResult } from '../common/interfaces';
 
 @injectable()
 export class FileSyncerManager {
@@ -19,6 +19,7 @@ export class FileSyncerManager {
   private readonly maxAttempts: number;
   private readonly taskPoolSize: number;
   private taskCounter: number;
+  private readonly logContext: LogContext;
 
   public constructor(
     @inject(SERVICES.LOGGER) private readonly logger: Logger,
@@ -50,10 +51,16 @@ export class FileSyncerManager {
     this.maxAttempts = this.config.get<number>('jobManager.task.maxAttempts');
     this.taskPoolSize = this.config.get<number>('fileSyncer.taskPoolSize');
     this.taskCounter = 0;
+
+    this.logContext = {
+      fileName: __filename,
+      class: FileSyncerManager.name,
+    };
   }
 
   @withSpanAsyncV4
   public async start(task: ITaskResponse<TaskParameters>): Promise<void> {
+    const logContext = { ...this.logContext, function: this.start.name };
     const spanActive = trace.getActiveSpan();
     spanActive?.setAttributes({
       [INFRA_CONVENTIONS.infra.jobManagement.taskId]: task.id,
@@ -66,9 +73,19 @@ export class FileSyncerManager {
     const taskResult = await this.handleTask(task);
     if (taskResult.completed) {
       await this.taskHandler.ack<IUpdateTaskBody<TaskParameters>>(task.jobId, task.id);
-      this.logger.info({ msg: 'Finished ack task', task: task.id, modelId: task.parameters.modelId });
+      this.logger.info({
+        msg: 'Finished ack task',
+        logContext,
+        task: task.id,
+        modelId: task.parameters.modelId,
+      });
       await this.deleteTaskParameters(task);
-      this.logger.info({ msg: `Deleted task's parameters successfully`, task: task.id, modelId: task.parameters.modelId });
+      this.logger.info({
+        msg: `Deleted task's parameters successfully`,
+        logContext,
+        task: task.id,
+        modelId: task.parameters.modelId,
+      });
     }
     if (workingTaskTimerEnd) {
       workingTaskTimerEnd();
@@ -78,6 +95,7 @@ export class FileSyncerManager {
     this.tasksGauge?.dec({ type: this.taskType });
     this.logger.info({
       msg: 'Done working on a task in this interval',
+      logContext,
       taskId: task.id,
       isCompleted: taskResult.completed,
       modelId: task.parameters.modelId,
@@ -94,29 +112,49 @@ export class FileSyncerManager {
 
   @withSpanAsyncV4
   private async handleFailedTask(task: ITaskResponse<TaskParameters>, taskResult: TaskResult): Promise<void> {
+    const logContext = { ...this.logContext, function: this.handleFailedTask.name };
     try {
       await this.updateIndexError(task, taskResult.index);
       // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
       await this.rejectJobManager(taskResult.error!, task);
-      this.logger.info({ msg: 'Updated failing the task in job manager', task: task.id });
+      this.logger.info({
+        msg: 'Updated failing the task in job manager',
+        logContext,
+        task: task.id,
+      });
     } catch (error) {
-      this.logger.error({ error, taskId: task.id, modelId: task.parameters.modelId });
+      this.logger.error({
+        error,
+        logContext,
+        taskId: task.id,
+        modelId: task.parameters.modelId,
+      });
     }
   }
 
   @withSpanAsyncV4
   private async handleTask(task: ITaskResponse<TaskParameters>): Promise<TaskResult> {
-    this.logger.debug({ msg: 'Starting handleTask', taskId: task.id });
+    const logContext = { ...this.logContext, function: this.handleTask.name };
+    this.logger.debug({
+      msg: 'Starting handleTask',
+      logContext,
+      taskId: task.id,
+    });
     const taskParameters = task.parameters;
     const taskResult: TaskResult = this.initTaskResult(taskParameters);
-
     while (taskResult.index < taskParameters.paths.length) {
       const filePath = taskParameters.paths[taskResult.index];
       try {
         await this.syncFile(filePath, taskParameters);
       } catch (error) {
         await this.handleFailedTask(task, taskResult);
-        this.logger.error({ error, taskId: task.id, modelId: task.parameters.modelId });
+        this.logger.error({
+          msg: 'failed to handle task',
+          error,
+          logContext,
+          taskId: task.id,
+          modelId: task.parameters.modelId,
+        });
         taskResult.error = error instanceof Error ? error : new Error(String(error));
         return taskResult;
       }
@@ -154,13 +192,23 @@ export class FileSyncerManager {
       return;
     }
 
-    this.logger.debug({ msg: 'Try to dequeue new task' });
+    const logContext = { ...this.logContext, function: this.fetch.name };
+
+    this.logger.debug({
+      msg: 'Try to dequeue new task',
+      logContext,
+    });
     const task = await this.taskHandler.dequeue<TaskParameters>(JOB_TYPE, this.taskType);
     if (!task) {
       return;
     }
 
-    this.logger.info({ msg: 'Found a task to work on!', task: task.id, modelId: task.parameters.modelId });
+    this.logger.info({
+      msg: 'Found a task to work on!',
+      logContext,
+      task: task.id,
+      modelId: task.parameters.modelId,
+    });
     this.taskCounter++;
     this.tasksGauge?.inc({ type: this.taskType });
     await this.start(task);
--- src/providers/nfsProvider.ts
+++ src/providers/nfsProvider.ts
@@ -4,35 +4,59 @@ import { Logger } from '@map-colonies/js-logger';
 import { withSpanAsyncV4 } from '@map-colonies/telemetry';
 import { inject, injectable } from 'tsyringe';
 import { Tracer } from '@opentelemetry/api';
-import { NFSConfig, Provider } from '../common/interfaces';
+import { LogContext, NFSConfig, Provider } from '../common/interfaces';
 import { SERVICES } from '../common/constants';
 
 @injectable()
 export class NFSProvider implements Provider {
+  private readonly logContext: LogContext;
+
   public constructor(
     @inject(SERVICES.LOGGER) private readonly logger: Logger,
     @inject(SERVICES.TRACER) public readonly tracer: Tracer,
     private readonly config: NFSConfig
-  ) {}
+  ) {
+    this.logContext = {
+      fileName: __filename,
+      class: NFSProvider.name,
+    };
+  }
 
   @withSpanAsyncV4
   public async getFile(filePath: string): Promise<Buffer> {
+    const logContext = { ...this.logContext, function: this.getFile.name };
     const pvPath = this.config.pvPath;
     const fullPath = `${pvPath}/${filePath}`;
-    this.logger.debug({ msg: 'Starting getFile', fullPath });
+    this.logger.debug({
+      msg: 'Starting getFile',
+      logContext,
+      fullPath,
+    });
     const data = await fs.promises.readFile(fullPath);
-    this.logger.debug({ msg: 'Done getFile' });
+    this.logger.debug({
+      msg: 'Done getFile',
+      logContext,
+    });
     return data;
   }
 
   @withSpanAsyncV4
   public async postFile(filePath: string, data: Buffer): Promise<void> {
+    const logContext = { ...this.logContext, function: this.postFile.name };
     const pvPath = this.config.pvPath;
     const fullPath = `${pvPath}/${filePath}`;
-    this.logger.debug({ msg: 'Starting postFile', fullPath });
+    this.logger.debug({
+      msg: 'Starting postFile',
+      logContext,
+      fullPath,
+    });
     const dir = path.dirname(fullPath);
     await fs.promises.mkdir(dir, { recursive: true });
     await fs.promises.writeFile(fullPath, data);
-    this.logger.debug({ msg: 'Done postFile', fullPath });
+    this.logger.debug({
+      msg: 'Done postFile',
+      logContext,
+      fullPath,
+    });
   }
 }
